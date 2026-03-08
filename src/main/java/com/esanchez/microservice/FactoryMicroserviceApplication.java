99b3e7a356245ecc52f08b3936ee97fe0ab6b050
Enrique Sanchez <esanchez.devel@gmail.com>
2026-03-08 13:16:40 +0100

    feat: create project

---
+++ src/main/java/com/esanchez/microservice/FactoryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.esanchez.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esanchez/microservice/FactoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esanchez.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
